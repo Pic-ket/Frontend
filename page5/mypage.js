1fdffbd936f23e6ef8efe54fd85184c503ea6e98
Taehoon Kim <twodf78@naver.com>
2023-05-21 07:34:41 +0900

    qus

--- page5/mypage.js
+++ page5/mypage.js
@@ -44,7 +44,7 @@ const callTokenUri = async () => {
   try {
     tokenId = await callGetTokenId();
     tokenUrl = await contract.methods.tokenURI(tokenId).call();
-    $(".image").attr("src", tokenUrl);
+    $("#cont_img").attr("src", tokenUrl);
 
     console.log(tokenUrl);
   } catch (err) {
